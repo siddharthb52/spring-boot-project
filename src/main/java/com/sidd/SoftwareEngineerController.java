262d41185c2400a85e815e873caa9ea81a320692
Siddharth Bawankule <siddharth.bawankule.52@gmail.com>
2025-05-28 00:07:26 -0700

    Added set up for spring boot SWE class, postgres connections, and docker

---
+++ src/main/java/com/sidd/SoftwareEngineerController.java
@@ -0,0 +1,22 @@
+package com.sidd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+}
